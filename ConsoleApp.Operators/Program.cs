47c454f267af7a6015f63448991be2055b94a351
Salena Calbert <Salenacalbert@gmail.com>
2025-02-17 10:42:05 -0500

    Added calculations

---
+++ ConsoleApp.Operators/Program.cs
@@ -0,0 +1,73 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.Write(" Please enter first number: ");
+int num1=Convert.ToInt32(Console.ReadLine());
+
+Console.Write(" Please enter second number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+//Arithmetic Operators
+
+int sum= num1 + num2;
+int minus= num1 - num2;
+int product= num1 * num2;
+int quotient = num1 / num2;
+int mod= num1 % num2;
+
+Console.WriteLine("******************* Maths Results*******************");
+Console.WriteLine("Sum is: " + sum);
+Console.WriteLine("Difference is: " + minus);
+Console.WriteLine("Product is: " + product);
+Console.WriteLine("Quotient is: " + quotient);
+Console.WriteLine("Modulus is: " + mod);
+Console.WriteLine("******************* EndMaths Results*******************");
+
+
+/* > greater than
+ * < less than
+ * == equal
+ * != not equal
+ * && and
+ * || or
+ */
+
+// Logic operations and operators use booleans
+
+var isGreaterThan = num1 > num2; // var is a wild card data type 
+bool isLessThan= num1 < num2;
+bool isEqualTo = num1 == num2;
+bool isGreaterThanOrEqualTo = num1 >= num2;
+bool isLessThanOrEqualTo = num1 <= num2;
+bool isNotEqualTo = num1!=num2;
+
+Console.WriteLine();
+Console.WriteLine("******************* Logics Results*******************");
+
+Console.WriteLine("Is greater than: " + isGreaterThan);
+Console.WriteLine("Is less than: " + isLessThan);
+Console.WriteLine("Is equal to: " + isEqualTo);
+Console.WriteLine("IS greater than or equal to: " + isGreaterThanOrEqualTo);
+Console.WriteLine("IS less than or equal to: " + isLessThanOrEqualTo);
+Console.WriteLine("Is Not equal to: " + isNotEqualTo);
+Console.WriteLine("******************* Logics Results*******************");
+
+//Assignment operations and Operators
+
+Console.WriteLine("******************* Assignment Results*******************");
+Console.WriteLine();
+
+num1 += 5;
+Console.WriteLine("Add 5" + num1);
+
+num1 -= 5;
+Console.WriteLine("minus 5" + num1);
+
+num1 /= 2;
+Console.WriteLine("divided by 2" + num1);
+
+num1 %= 2;
+Console.WriteLine("modulous 2" + num1);
+
+num1 *= 10;
+Console.WriteLine("multiply by 10" + num1);
+
--- ConsoleApp.VariablesDemo/Program.cs
+++ ConsoleApp.VariablesDemo/Program.cs
@@ -12,30 +12,45 @@ Console.WriteLine(age+retirementAge);
 
 bool isRetired=false;
 Console.WriteLine("is he retired?" + isRetired);
+Console.WriteLine();
 
 // declare vaiables
-string Fname= string.Empty; // give variable default value 
+string? Fname= string.Empty; // string? allows the variable to be null, store nothing 
 string Lname = string.Empty;
 int User_age = 0;
+decimal salary = 0;
+char gender = char.MinValue;
+bool working= false;
 
 // prompt user for input
-Console.WriteLine("Enter your first name: ");
+Console.Write("Enter your first name: ");
 Fname = Console.ReadLine(); // receive imput from user
-Console.WriteLine("Enter your last name: ");
-Lname= Console.ReadLine();
+Console.Write("Enter your last name: ");
+Lname = Console.ReadLine();
 
 
-Console.WriteLine("Enter your age: ");
+Console.Write("Enter your age: ");
 User_age = Convert.ToInt32(Console.ReadLine()); // input entered is a string have to convert it 
 
+Console.Write("Enter salary: ");
+salary= Convert.ToDecimal(Console.ReadLine());
+Console.Write("Enter gender: (M of F) " );
+gender=Convert.ToChar(Console.ReadLine());
+Console.Write("Are you working? Trur or False ");
+working= Convert .ToBoolean(Console.ReadLine());
+
+//process the data
 int workingAge = retirementAge - User_age;
 
 // output the information 
-Console.WriteLine("Full name:" + Fname + Lname);
+Console.WriteLine("Full name:" + Fname + " " + Lname);
 Console.WriteLine("Age:" + User_age);
-Console.WriteLine("Working years remaining:" + workingAge);
+Console.WriteLine("Working years remaining:  " + workingAge);
+Console.WriteLine("Salary " + salary);
+Console.WriteLine("Gender " + gender);
+Console.WriteLine("Working " + working);
 
-
+//refactor means change code 
 
 
 
